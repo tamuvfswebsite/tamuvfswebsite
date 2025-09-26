873c15f8b174cfb06e09a3ac7cd89ccf49bb0de0
Eric Peterson <eaptrsn@gmail.com>
2025-09-25 19:34:06 -0500

    Big push but a lot of it is stuff that was preconfigured wrong in the professor provided repo.
    Change manifest:
      -Fixed javascript precompiling/bundling (old rails 6 version was used which broke anything on the website that required javascript to work)
      - Updated some user stuff because I was dumb and put my important configuration changes on the branch I was already working on.
      - Removed "Posts" and "Comments" stuff including database migrations that drop tables (those tables were there from the repo we used as a starting point)
      - Added a basic AI generated home page because the old home page was part of posts/comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 20_250_925_181_430) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_25_190000) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'pg_catalog.plpgsql'
+  enable_extension "pg_catalog.plpgsql"
 
-  create_table 'admins', force: :cascade do |t|
-    t.string 'email', null: false
-    t.string 'full_name'
-    t.string 'uid'
-    t.string 'avatar_url'
-    t.datetime 'created_at', precision: nil, null: false
-    t.datetime 'updated_at', precision: nil, null: false
-    t.index ['email'], name: 'index_admins_on_email', unique: true
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
-  create_table 'comments', force: :cascade do |t|
-    t.bigint 'post_id', null: false
-    t.text 'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['post_id'], name: 'index_comments_on_post_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "google_uid"
+    t.string "google_avatar_url"
+    t.index ["google_uid"], name: "index_users_on_google_uid", unique: true
   end
-
-  create_table 'posts', force: :cascade do |t|
-    t.text 'title'
-    t.text 'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.integer 'views'
-    t.integer 'clicks'
-    t.datetime 'last_reviewed_at', precision: nil
-    t.index ['user_id'], name: 'index_posts_on_user_id'
-  end
-
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'email'
-    t.string 'role'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'google_uid'
-    t.string 'google_avatar_url'
-    t.index ['google_uid'], name: 'index_users_on_google_uid', unique: true
-  end
-
-  add_foreign_key 'comments', 'posts'
 end
