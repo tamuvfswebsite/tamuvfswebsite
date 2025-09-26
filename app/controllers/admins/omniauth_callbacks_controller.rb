40263f014e4ae4f3b9656597f2d998a8ffff78ff
mkelsay22 <mkelsay22@tamu.edu>
2025-09-26 13:58:56 -0500

    homepage fix

--- app/controllers/admins/omniauth_callbacks_controller.rb
+++ app/controllers/admins/omniauth_callbacks_controller.rb
@@ -8,8 +8,7 @@ class Admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if admin.present?
       sign_out_all_scopes
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in admin, event: :authentication
-      redirect_to homepage_path
+      sign_in_and_redirect admin, event: :authentication
     else
       flash[:alert] =
         t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
