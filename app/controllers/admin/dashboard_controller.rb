133257e1e1bc9dfb390ba62f313dac11ca8f7e52
LukeConran <stlsoccerdude@gmail.com>
2025-09-28 11:58:09 -0500

    Added some necessary links, revamped admin dashboard slightly

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < Admin::BaseController
   def index
-    # @total_users = User.count
+    @total_users = User.count
     # @upcoming_events = Event.where('date >= ?', Time.current).order(:date).limit(5)
     # @recent_applications = Application.order(created_at: :desc).limit(5)
     # @resume_count = Resume.count
