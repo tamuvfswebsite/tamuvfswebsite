475156f02d93c6ee4461b3e6a61c329c100d3432
LukeConran <stlsoccerdude@gmail.com>
2025-09-28 16:01:53 -0500

    Configured time zone. Added recent activity functionality, move links for more consistent navigation

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class Admin::DashboardController < Admin::BaseController
   def index
     @total_users = User.count
-    # @upcoming_events = Event.where('date >= ?', Time.current).order(:date).limit(5)
+    @total_events = Event.count
+    @upcoming_events = Event.where('date >= ?', Time.current).order(:date).limit(5)
+    @recent_events = Event.order(created_at: :desc).limit(5)
     # @recent_applications = Application.order(created_at: :desc).limit(5)
     # @resume_count = Resume.count
   end
