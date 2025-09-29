019f4fd4eeb9dd7dd4718930efb319231bc97cb8
Luke Conran <145418221+LukeConran@users.noreply.github.com>
2025-09-29 13:05:53 -0500

    Delete deprecated function in home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_admin!, only: [:index]
   def index
     # Simple index page - no authentication required for viewing
   end
