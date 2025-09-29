6742a41b0b6933029260b94f9ca5485710d942a7
Julius Lee <juliuslee3929@gmail.com>
2025-09-29 16:50:13 -0500

    Rubocop final

--- db/migrate/20250926184741_create_active_storage_tables.active_storage.rb
+++ db/migrate/20250926184741_create_active_storage_tables.active_storage.rb
@@ -1,5 +1,6 @@
 # This migration comes from active_storage (originally 20170806125915)
 class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def change
     # Use Active Record's configured type for primary and foreign keys
     primary_key_type, foreign_key_type = primary_and_foreign_key_types
@@ -56,4 +57,5 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
     foreign_key_type = setting || :bigint
     [primary_key_type, foreign_key_type]
   end
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 end
