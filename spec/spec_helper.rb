97a6bd4746d8dd6f9473af4450e0c814af1d5710
Julius Lee <juliuslee3929@gmail.com>
2025-10-17 16:51:44 -0500

    simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,15 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/spec/'
+  add_group 'Models', 'app/models'
+  add_group 'Controllers', 'app/controllers'
+  # optional: coverage_dir 'coverage' # default is coverage/
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
