4f6231cd3120f7e6b5169218781d74ac78cb2e06
Eric Peterson <eaptrsn@gmail.com>
2025-11-10 20:10:49 -0600

    Rubocop Fix

--- db/migrate/20251016000000_convert_to_many_to_many_organizational_roles.rb
+++ db/migrate/20251016000000_convert_to_many_to_many_organizational_roles.rb
@@ -1,38 +1,50 @@
 class ConvertToManyToManyOrganizationalRoles < ActiveRecord::Migration[7.2]
   def up
-    # Create organizational_roles table if it doesn't exist
-    unless table_exists?(:organizational_roles)
-      create_table :organizational_roles do |t|
-        t.string :name, null: false
-        t.text :description
-        t.timestamps
-      end
-      add_index :organizational_roles, :name, unique: true
-    end
+    create_organizational_roles_table_if_needed
+    create_junction_table
+    migrate_existing_data
+    remove_old_column
+  end
 
-    # Create the junction table
+  private
+
+  def create_organizational_roles_table_if_needed
+    return if table_exists?(:organizational_roles)
+
+    create_table :organizational_roles do |t|
+      t.string :name, null: false
+      t.text :description
+      t.timestamps
+    end
+    add_index :organizational_roles, :name, unique: true
+  end
+
+  def create_junction_table
     create_table :organizational_role_users do |t|
       t.references :user, null: false, foreign_key: true
       t.references :organizational_role, null: false, foreign_key: true
       t.timestamps
     end
 
-    # Add unique index to prevent duplicate assignments
     add_index :organizational_role_users, %i[user_id organizational_role_id],
               unique: true, name: 'index_org_role_users_on_user_and_role'
+  end
 
-    # Migrate existing data
+  def migrate_existing_data
     User.where.not(organizational_role_id: nil).find_each do |user|
       execute <<-SQL
         INSERT INTO organizational_role_users (user_id, organizational_role_id, created_at, updated_at)
         VALUES (#{user.id}, #{user.organizational_role_id}, NOW(), NOW())
       SQL
     end
+  end
 
-    # Remove the old foreign key and column
+  def remove_old_column
     remove_foreign_key :users, :organizational_roles
     remove_column :users, :organizational_role_id
   end
+
+  public
 
   def down
     # Add the column back
@@ -55,7 +67,7 @@ class ConvertToManyToManyOrganizationalRoles < ActiveRecord::Migration[7.2]
     # Drop the junction table
     drop_table :organizational_role_users
 
-    # Note: We don't drop organizational_roles table in down migration
+    # NOTE: We don't drop organizational_roles table in down migration
     # because it may have been created by a different migration
   end
 end
